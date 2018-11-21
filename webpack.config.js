2ebc0436d43cef2bf82f70c42857e61597ce49a5
Robert Foy <robert.foy@gmail.com>
2018-11-22 03:40:37 +1100

    Added babel-loader

--- webpack.config.js
+++ webpack.config.js
@@ -9,5 +9,14 @@
     },
     optimization: {
       minimize: true
+    },
+    module: {
+      rules: [{
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }]
     }
   }
